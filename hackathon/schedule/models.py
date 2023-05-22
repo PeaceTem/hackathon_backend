e993265bc47ea8951bdd58827b7ab569850b760f
PeaceTem <olumidejonathan10@gmail.com>
2023-05-22 01:45:48 +0100

    column added

--- hackathon/schedule/models.py
+++ hackathon/schedule/models.py
@@ -1,22 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class CourseCode(models.Model):
     title = models.CharField(max_length=100)
     code = models.CharField(max_length=10)
-    
+    student_population = models.PositiveSmallIntegerField(default=1)
     def __str__(self):
         return f"{self.code}"
 
 
+
 class Venue(models.Model):
     name = models.CharField(max_length=100)
-
+    capacity = models.PositiveSmallIntegerField(default=1)
     def __str__(self):
         return f"{self.name}"
 
+
+
+
+#format this later using the techniques learnt in csc231 :2d
 class TimeSlot(models.Model):
     HOURS = (
         zip(range(0,24), range(0,24))
@@ -42,14 +48,18 @@ class TimeSlot(models.Model):
 
     @property
     def start_time(self):
-        return f"{self.start_hour}:{self.start_minute}"
+        return f"{self.start_hour:02d}:{self.start_minute:02d}"
 
     @property
     def end_time(self):
-        return f"{self.end_hour}:{self.end_minute}"
+        return f"{self.end_hour:02d}:{self.end_minute:02d}"
 
     
+    def __str__(self):
+        return f"{self.start_time} - {self.end_time}"
+
     
+
 class Day(models.Model):
     DAYS = (
         ("Monday", "Monday"),
@@ -69,8 +79,12 @@ class Day(models.Model):
     day = models.CharField(max_length=50, choices=DAYS)
     week = models.CharField(max_length=50, choices=WEEKS)
 
+
+
     def __str__(self):
         return f"{self.day}"
+
+
 
 
 class Supervisor(models.Model):
@@ -86,13 +100,33 @@ class Supervisor(models.Model):
 
 class Column(models.Model):
     course_code = models.OneToOneField(CourseCode, on_delete=models.CASCADE, related_name="column")
-    time_slot = models.OneToOneField(TimeSlot, on_delete=models.CASCADE, related_name="column")
-    venue = models.OneToOneField(Venue, on_delete=models.CASCADE, related_name="column")
+    time_slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE, related_name="column")
+    venue = models.ForeignKey(Venue, null=True, blank=True, on_delete=models.CASCADE, related_name="column")
 
 
     def __str__(self):
         return f"{self.course_code}, {self.time_slot}, {self.venue}"
 
+
+    def create(self, **obj):
+        venues = Venue.objects.order_by("capacity")
+        for v in venues:
+            if obj['course_code'].student_population <= v.capacity:
+                obj['venue'] = v
+                break
+
+        return super().create(self, **obj)
+
+
+    def save(self, *args, **kwargs):
+        venues = Venue.objects.order_by("capacity")
+        for v in venues:
+            if self.course_code.student_population <= v.capacity:
+                self.venue = v
+                break
+
+        return super().save(*args, **kwargs)
+        
 
 
 class Row(models.Model):
@@ -106,12 +140,13 @@ class Row(models.Model):
 
 
 class Cell(models.Model):
-    VALUES = (zip(-1,2), zip(-1,2))
+    VALUES = (zip(range(-1,2) , range(-1,2)))
     
     column = models.ForeignKey(Column, on_delete=models.CASCADE, related_name="cell")
     row = models.ForeignKey(Row, on_delete=models.CASCADE, related_name="cell")
-    value = models.SmallIntegerField(choices=VALUES)
-
+    value = models.SmallIntegerField(choices=VALUES, default=0)
+    i = models.PositiveSmallIntegerField()
+    j = models.PositiveSmallIntegerField()
     def __str__(self):
         return f"({self.row}, {self.column})"
     
@@ -120,3 +155,4 @@ class Cell(models.Model):
 
 
 
+# Create Pastor Adeshida PowerPoint Presentation

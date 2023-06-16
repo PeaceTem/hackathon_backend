987b252b523cd028e9a65a5e8132869b02508884
PeaceTem <olumidejonathan10@gmail.com>
2023-06-16 04:03:42 +0100

    hosted on vercel

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-1s$sq*ou^3rrbk_vq2!xmi=__+6-@s1#b-(y&g6*s6!8d7x_0q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

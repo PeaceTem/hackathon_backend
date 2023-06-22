d6f9ac3e51e685ee1460d644fa83bf76648406bb
PeaceTem <olumidejonathan10@gmail.com>
2023-06-22 02:04:55 +0100

    modified the gitignore file

--- hackathon/department/models.py
+++ hackathon/department/models.py
@@ -3,7 +3,7 @@ from faculty.models import Faculty
 # Create your models here.
 
 class Department(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=120)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, null=True, blank=True, related_name='departments')
 
 
